5c8bca30c4141ce2d26a0657e4081ce4b5b73d42
celsops <celsovasconcelos.27@gmail.com>
2019-05-26 12:34:13 -0300

    Add Footer

--- alagoasNoticasGit/paginas/historia.php
+++ alagoasNoticasGit/paginas/historia.php
@@ -30,6 +30,10 @@
       .content-head__title{
         font-size:26px;
       }
+      .icon{
+            width:24px;
+            height:24px;
+        }
   </style>
 </head>
 <body>
@@ -56,7 +60,7 @@
       <ul class="sidenav" id="mobile-demo">
         <li><a href="../index.php">Início</a></li>
         <li><a href="./noticias.php">Notícias publicadas</a></li>
-        <li><a href="./historia.php">Histórias</a></li>
+        <li><a href="./historia.php">História</a></li>
         <li><a href="./custoVida.php">Custo de vida</a></li>
       </ul>
       <script>
@@ -67,12 +71,44 @@
       </script>  
     </div>
   <?php
-    include_once '../../phpQuery/phpQuery/phpQuery.php';
+    include_once '../phpQuery/phpQuery/phpQuery.php';
     $dominio = "https://pt.wikipedia.org/wiki/Hist%C3%B3ria_de_Alagoas";
     phpQuery::newDocumentFileHTML($dominio);
     $titulo  = pq('.firstHeading');
     $conteudo = pq('.mw-body-content');
     echo '<div class="conteudo">'.$titulo.$conteudo. '</div>';
   ?>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                <h5 class="white-text">Informações do desenvolvedor</h5>
+                <p class="grey-text text-lighten-4">Este site foi desenvolvido por Celso Leandro Nascimento Vasconcelos. Todas as informações aqui contidas são totalmente véridicas.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Redes sociais do desenvolvedor</h5>
+                <ul>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.facebook.com/celsolnv">
+                        <img class="icon" src="../icon/facebook.svg" alt="facebook">
+                        <span style="font-size:24px;">Facebook</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.instagram.com/celsolv/">
+                        <img class="icon" src="../icon/instagram.svg" alt="instagram">
+                        <span style="font-size:24px;">Instagram</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.linkedin.com/in/celso-vasconcelos-b3a793143/">
+                        <img class="icon" src="../icon/linkedin.svg" alt="linkedin">
+                        <span style="font-size:24px;">Linkedin</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://github.com/celsops">
+                        <img class="icon" src="../icon/github.svg" alt="github">
+                        <span style="font-size:24px;">Github</span>
+                    </a></li>
+                </ul>
+                </div>
+            </div>
+        </div>
+</footer>
+           
 </body>
 </html>
--- alagoasNoticasGit/paginas/noticias.php
+++ alagoasNoticasGit/paginas/noticias.php
@@ -30,6 +30,10 @@
     .content-head__title{
       font-size:26px;
     }
+    .icon{
+          width:24px;
+          height:24px;
+      }
   </style>
 </head>
 <body>
@@ -57,7 +61,7 @@
     <ul class="sidenav" id="mobile-demo">
       <li><a href="../index.php">Início</a></li>
       <li><a href="./">Notícias publicadas</a></li>
-      <li><a href="./historia.php">Histórias</a></li>
+      <li><a href="./historia.php">História</a></li>
       <li><a href="./custoVida.php">Custo de vida</a></li>
       
     </ul>
@@ -119,5 +123,36 @@
       echo "</div>";
       
     ?>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                <h5 class="white-text">Informações do desenvolvedor</h5>
+                <p class="grey-text text-lighten-4">Este site foi desenvolvido por Celso Leandro Nascimento Vasconcelos. Todas as informações aqui contidas são totalmente véridicas.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Redes sociais do desenvolvedor</h5>
+                <ul>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.facebook.com/celsolnv">
+                        <img class="icon" src="../icon/facebook.svg" alt="facebook">
+                        <span style="font-size:24px;">Facebook</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.instagram.com/celsolv/">
+                        <img class="icon" src="../icon/instagram.svg" alt="instagram">
+                        <span style="font-size:24px;">Instagram</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.linkedin.com/in/celso-vasconcelos-b3a793143/">
+                        <img class="icon" src="../icon/linkedin.svg" alt="linkedin">
+                        <span style="font-size:24px;">Linkedin</span>
+                    </a></li>
+                    <li><a target="_blank" class="grey-text text-lighten-3" href="https://github.com/celsops">
+                        <img class="icon" src="../icon/github.svg" alt="github">
+                        <span style="font-size:24px;">Github</span>
+                    </a></li>
+                </ul>
+                </div>
+            </div>
+        </div>
+</footer>
 </body>
 </html> 
